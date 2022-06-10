dd385d443a5daf280086362f5c6893fff046b3a5
brandonmizutani <bran2miz@gmail.com>
2022-06-10 12:35:56 -0700

    hangman

--- rock_paper_scissors/hangman.py
+++ rock_paper_scissors/hangman.py
@@ -1,5 +1,19 @@
 import random
 from random_generator import words
+import string
 
 def get_valid_words():
-  pass
+  word = random.choice(words)
+
+  while '-' in word or ' ' in word:
+    word = random.choice(words)
+  
+  return word
+
+def hangman():
+  word = get_valid_words(word)
+  word_letters = set(word) 
+  # variable that saves letters in a word
+  alphabet = set(string.ascii_uppercase)
+  used_letters = set()
+  # what the user has already guessed.

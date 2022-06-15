df3c5c7401f3564a5a9305409cf94c58ec7539a6
brandonmizutani <bran2miz@gmail.com>
2022-06-14 17:49:31 -0700

    got some of the features within rock paper scissors complete

--- rock_paper_scissors/tick-tac-toe.py/player.py
+++ rock_paper_scissors/tick-tac-toe.py/player.py
@@ -10,5 +10,20 @@ class Player:
     pass
 
 class RandomComputerPlayer(Player):
+
   def __init__(self, letter):
+    # initalize the superclass
+    super().__init__(letter)
+    # call the initialization in the super class which is the player
+  
+  def get_move(self, game):
+    square = random.choice(game.available_moves())
+    return square
+
+class HumanPlayer(Player):
+
+  def __init__(self, letter):
+    super().__init__(letter)
+  
+  def get_move(self, game):
     pass
---
+++ rock_paper_scissors/tick-tac-toe.py/tic_tac_toe.py
@@ -0,0 +1,40 @@
+
+class TicTacToe:
+  def __init__(self):
+    self.board = [' ' for i in range(9)]
+    # single list to represent the 3 x 3 tic tac toe board
+    self.current_winner = None
+    # this will keep track whether there is a current winner of this game
+  
+  def print_board(self):
+    for row in [self.board[i * 3: (i + 1) * 3] for i in range(3)]:
+      # choosing which row:
+      # ie 
+      # 0: 0*3 = 0: (0 + 1 = 1) * 3 (O -> 3)
+      # 1: 1*3 = 3: (1 + 1 = 2) * 3 (3 -> 6)
+      # 2: 2*3 = 6: (2 + 1 = 3) * 3 (6 -> 9)
+      print('| ' + ' | '.join(row) + ' |')
+  
+  @staticmethod
+  # static method because it doesn't pass in a self, it will print which numbers correspond to which spot.
+  def print_board_nums():
+    # 0 | 1 | 2 etc (tell what number will correspond to what spot)
+    number_board = [[str(i) for i in range(j*3, (j + 1) * 3)] for j in range(3)]
+    # '| 0 | 1 | 2 |' index j[0]
+    # '| 3 | 4 | 5 |' index j[1]
+    # '| 6 | 7 | 8 |' index j[2]
+    for row in number_board:
+      print('| ' + ' | '.join(row) + ' |')
+  
+  def available_moves(self):
+    moves = []
+    for (i, spot) in enumerate(self.board):
+    # enumerate creates a lists and assign tuples, that have the index and the value.
+    # ie ['x', 'x', 'o'] -> [(0, 'x'), (1, 'x'), (2, 'o')]
+      if spot == ' ':
+        moves.append(i)
+    return moves
+    # List Comprehension:
+    # return[i for i, spot in enumerate(self.board) if spot == ' ']
+
+
